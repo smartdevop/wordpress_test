030379ddb5cdb0b131cabfcb7e3a7d54a37adb72
Luc <lucpx@vnpt.vn>
2019-12-07 16:34:20 +0700

    #comment Update codeceptjs uploadphoto test

--- test/scenario/03_uploadphoto_test.js
+++ test/scenario/03_uploadphoto_test.js
@@ -1,10 +1,10 @@
-Feature('Upload Photo');
+1Feature('Upload Photo');
 let faker = require('faker');  //Thư viện sinh dữ liệu giả ngẫu nhiên
 
 Scenario('Login', (I) => {  
   I.amOnPage('/wp-login.php');
   I.fillField('input#user_login', "user");
-  I.fillField('input#user_pass', 'bitnami');
+  I.fillField('input#user_pass', 'bitnami1');
   I.click('input#wp-submit');
   I.seeInCurrentUrl('/wp-admin/');
 
